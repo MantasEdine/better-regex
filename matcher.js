11a72a21f5ef7feb52f9243d7dd5aa0e6bb45e89
MantasEdine <youcefmantas3@gmail.com>
2026-08-13 01:14:48 +0100

    stage one done

--- matcher.js
+++ matcher.js
@@ -1,46 +1,27 @@
 "use strict"
 const { Parser } = require('./parser.js')
 
-
-function Matcher(input, exp){
-    let match = 0
-
-
-    let res = Parser(exp)
-    console.log(res)
-    console.log("im here4")    
-    if(res.type === "concat"){
-        let str = ""
-        let i = 0
-        let arr = res.children
-        console.log(arr)
-        while(i < arr.length){
-               
-            str += arr[i].value
-            i++
-
+function matchNode(node, input, pos) {
+    if (node.type === "char") {
+        if (input[pos] === node.value) return pos + 1
+        return -1
+    }
+    if (node.type === "dot") {
+        if (pos < input.length) return pos + 1
+        return -1
+    }
+    if (node.type === "concat") {
+        for (let child of node.children) {
+            pos = matchNode(child, input, pos)   
+            if (pos === -1) return -1
         }
-        console.log(str)
-        console.log(str[0])
-       let j = 0 
-        while (j < input.length){
-        
-            if(input[j] === str[0] && input.slice(j,j + str.length) === str){
-                match++
-                console.log("one_shot")
-                j += str.length
-            }else {
-                j++
-            }
-            
-        }
-
-        return match
-    }else {
-        return null
-    } 
-
+        return pos
+    }
+}
+function match(pattern, input) {
+    const tree = Parser(pattern)
+    const end = matchNode(tree, input, 0)
+    return end === input.length
 }
 
-const result = Matcher("cat","c.t")
-console.log(result)
+module.exports = { match }
--- parser.js
+++ parser.js
@@ -3,55 +3,34 @@
 
 function Parser(exp){
 
-
-    function isRegexSpecial(ch) {
+  function isRegexSpecial(ch) {
          const specials = ['\\', '^', '$', '.', '|', '?', '*', '+', '(', ')', '[', ']', '{', '}']
          return specials.includes(ch)
    }
-    let operation_type = 0
-    for(let i = 0 ; i<exp.length ; i++){
-        if(isRegexSpecial(exp[i])){
-                
-            operation_type += 1
-            
-        }
-
-    }
-    console.log(operation_type)
-    
-        console.log("im here1")
-
-  function basicConcat(exp){
-
-            console.log("im here3")
-                let charArr = []
-
-
-     function isLetter(ch) {
+  function isLetter(ch) {
          const code = ch.charCodeAt(0)
-         return (code >= 0x41 && code <= 0x5A) ||  // A-Z (65-90)
+         return (code >= 0x41 && code <= 0x5A) ||  
                 (code >= 0x61 && code <= 0x7A) ||
                 (code >= 0x30 && code <= 0x39)
-     }
-    let i = 0
-    while(i < exp.length && isLetter(exp[i])){
-            
-        const Char = {type : "char", value : exp[i]}
-        charArr.push(Char)
-        i++
-            
-    }
-      const expression = {type : "concat" , children : charArr}
-        console.log(expression)
-        return expression    
-  }
-    if(operation_type === 0){
-        console.log("im here2")
-        return basicConcat(exp)
-    }else {
-        return "null"
-    }    
+   }
 
+  function basicConcat(exp) {
+    let charArr = []
+    let i = 0
+    while (i < exp.length) {           
+        if (isLetter(exp[i])) {
+            charArr.push({type:"char", value: exp[i]})
+            i++
+        } else if (exp[i] === ".") {
+            charArr.push({type:"dot"})
+            i++                         
+        } else {
+            return "not yet!"
+        }
+    }                               
+    return {type:"concat", children: charArr}   
+   }
+   return basicConcat(exp)          
 }     
 
 
